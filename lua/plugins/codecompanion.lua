99d6c5801aae7cbca98386d14922973400e816ed
grmm <anton.minashkin@outlook.com>
2025-10-13 22:23:35 -0400

    feat: add codecompanion.nvim plugin
    Install and configure codecompanion.nvim to use Gemini CLI via Agent Client Protocol.

---
+++ lua/plugins/codecompanion.lua
@@ -0,0 +1,23 @@
+return {
+  "olimorris/codecompanion.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  config = function()
+    require("codecompanion").setup({
+      adapters = {
+        gemini_cli = function()
+          return require("codecompanion.adapters").extend("gemini", {
+            client = "gemini-cli",
+          })
+        end,
+      },
+      strategies = {
+        agent = {
+          adapter = "gemini_cli",
+        },
+      },
+    })
+  end,
+}
