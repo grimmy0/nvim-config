0fac470884eff96d893f1de13fe5479fb8ccd9c5
anton.minashkin <anton.minashkin@outlook.com>
2026-01-22 22:32:57 -0500

    fix(treesitter): use require('nvim-treesitter').setup

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,9 +3,11 @@ return {
   lazy = false,
   build = ':TSUpdate',
   config = function()
-    require('nvim-treesitter.configs').setup({
+    -- New setup for nvim-treesitter (without configs module)
+    require('nvim-treesitter').setup({
       ensure_installed = { "c", "lua", "vim", "python", "javascript", "json", "yaml", "markdown", "markdown_inline" },
       auto_install = true,
+      sync_install = false,
       highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
