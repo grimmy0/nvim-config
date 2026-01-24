8cc16eee4be185c0f3f1d02758e8eb61aa239f93
anton.minashkin <anton.minashkin@outlook.com>
2026-01-23 21:01:45 -0500

    Load telescope dap extension

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,5 +3,9 @@ return {
   dependencies = { {'nvim-lua/plenary.nvim'} },
   config = function()
     vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
+    local ok, telescope = pcall(require, 'telescope')
+    if ok then
+      pcall(telescope.load_extension, 'dap')
+    end
   end
 }
