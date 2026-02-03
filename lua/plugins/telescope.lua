59db3c29d86a07f5c9806fdd0dda806f00bbe419
anton.minashkin <anton.minashkin@outlook.com>
2026-02-02 23:28:18 -0500

    Add live grep keymap for Telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,6 +3,7 @@ return {
   dependencies = { {'nvim-lua/plenary.nvim'} },
   config = function()
     vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
+    vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
     local ok, telescope = pcall(require, 'telescope')
     if ok then
       pcall(telescope.load_extension, 'dap')
