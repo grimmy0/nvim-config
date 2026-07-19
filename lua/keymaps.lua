af8731024a0b8098de6e6583b5ed9ac83fa679c0
anton.minashkin <anton.minashkin@outlook.com>
2026-07-19 01:27:46 -0400

    refactor(keymaps): consolidate telescope maps into spec keys
    <leader>fg was bound in both keymaps.lua and the telescope config
    (last one loaded silently won), and keymaps.lua force-required
    telescope.builtin at startup. All three pickers now live in the
    spec's keys table; keymaps.lua keeps only non-plugin maps.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,7 +1,3 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-
 vim.keymap.set('n', '<leader>bn', '<cmd>bnext<cr>')
 vim.keymap.set('n', '<leader>bp', '<cmd>bprev<cr>')
 vim.keymap.set('n', '<leader>bb', '<cmd>b#<cr>')
